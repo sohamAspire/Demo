04802b48e087ca6e4bd2ce53c533a20653cb0c48
mitenAspire <125381689+mitenAspire@users.noreply.github.com>
2023-05-10 19:14:04 +0530

    Passsword filed

--- index.js
+++ index.js
@@ -1,49 +1,87 @@
-import React, { useState } from "react";
-import axios from "axios";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
-
-const Forgotpassword = () => {
-  const router = useRouter();
-  // const router = useRouter();
-
-  const [user, setuser] = useState();
-  const { register, errors, handleSubmit, reset } = useForm({});
-  const onSubmit = async (data) => {
-    console.log(data.email);
-    await axios
-      .get(`http://localhost:3001/login?data.Username=${data.email}`)
-      .then((result) => {
-        console.log(result.data[0].data.password);
-        setuser(result.data[0]);
-        console.log(result.data[0].id);
-        // console.log(router.query);
-      });
-  };
-  if (user) {
-    router.push({
-      pathname: `/forgotpassword/${user.id}`,
-      // query: { keyword: 'this way' },  
+import { yupResolver } from "@hookform/resolvers/yup";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import { Divider } from "primereact/divider";
+import { Password } from "primereact/password";
+import * as yup from "yup";
+import { useState } from "react";
+import { AiFillCheckCircle } from "react-icons/ai";
+export default function Home() {
+  const schema = yup.object().shape({
+    password: yup
+      .string()
+      .min(12)
+      .max(20)
+      // .required("Please Enter your password")
+      .matches(/^(?=.*[a-z])/, "Must contain at least one lowercase character")
+      .matches(/^(?=.*[A-Z])/, "Must contain at least one uppercase character")
+      .matches(/^(?=.*[0-9])/, "Must contain at least one number")
+      .matches(/^(?=.*[!@#%&_-])/, "Must contain at least one special character"),
   });
-  } else {
-    console.log("error");
-  }
-  return (
-    <container className="mt-5 w-50" bg="secondary">
-      <div border="info" className="p-4">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="email"
-            {...register("email")}
-            placeholder="Enter email"
-          />
-          <button type="submit" className="mx-2">
-            Submit
-          </button>
-        </form>
-      </div>
-    </container>
+  const { register, errors, handleSubmit, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const [value, setValue] = useState("");
+  const header = <div className="font-bold mb-3">Pick a password</div>;
+  const footer = (
+    <>
+      <Divider />
+      <p className="mt-2">Suggestions</p>
+      <ul className="pl-2 ml-2 mt-0 line-height-3">
+        <li className={value.match(/[a-z]/) ? "text-green-700" : "text-white"}>
+          {" "}
+          {value.match(/[a-z]/) ? (
+            <AiFillCheckCircle className={`text-green-700 m-2 inline`} />
+          ) : null}{" "}
+          At least one lowercase
+        </li>
+        <li className={value.match(/[A-Z]/) ? "text-green-700" : "text-white"}>
+          {value.match(/[A-Z]/) ? (
+            <AiFillCheckCircle className={`text-green-700 m-2 inline`} />
+          ) : null}
+          At least one uppercase
+        </li>
+        <li className={value.match(/[0-9]/) ? "text-green-700" : "text-white"}>
+          {value.match(/[0-9]/) ? (
+            <AiFillCheckCircle className={`text-green-700 m-2 inline`} />
+          ) : null}
+          At least one numeric
+        </li>
+        <li
+          className={value.match(/[!@#%&]/) ? "text-green-700" : "text-white"}
+        >
+          {value.match(/[!@#%&]/) ? (
+            <AiFillCheckCircle className={`text-green-700 m-2 inline`} />
+          ) : null}
+          Minimum 12 characters
+        </li>
+      </ul>
+    </>
   );
-};
-
-export default Forgotpassword;
+  const onSubmit = (data) => {
+    console.log({ data });
+    console.log(errors);
+    // reset();
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Password
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          header={header}
+          footer={footer}
+        ></Password>
+        {errors?.password && (
+          <p role="alert" className="text-red-700">
+            {errors?.password.message}
+          </p>
+        )}
+        <button type="submit" className=" mx-2">
+          submit
+        </button>
+      </form>
+    </>
+  );
+}
